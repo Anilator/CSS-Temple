7f5adb6832cbe4a382cd72a5c38a25046bb6bdcb
Evgenii Klimov <disturh@gmail.com>
2018-04-29 22:21:23 +0300

    Split header page added

--- src/components/layout/Body/Body.js
+++ src/components/layout/Body/Body.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 export default class Body extends Component {
   render() {
+    const children = this.props.children.filter(el => el);
     return (
       <div className="app__body">
         {
-          this.props.children.map((child, i) => (
+          children.map((child, i) => (
             <div className="app__container" key={i}>
               {child}
             </div>
--- src/components/pages/PageSplitHeader/PageSplitHeaderPresent.js
+++ src/components/pages/PageSplitHeader/PageSplitHeaderPresent.js
@@ -4,7 +4,12 @@ export default class PageSplitHeaderPresent extends React.Component {
   render() {
     return (
       <div className="PageSplitHeader">
-        PageSplitHeader
+        <div className="header-split">
+          <h2>Some split header</h2>
+        </div>
+        <div className="header-split">
+          <h2>Some multiline <br /> split header</h2>
+        </div>
       </div>
     );
   }
